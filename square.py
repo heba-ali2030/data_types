e97cbd0665addd2bbbe582fd4998de7c8ad3c0a0
heba-ali2030 <heba.galaa@gmail.com>
2021-02-13 00:31:46 +0200

    add

---
+++ square.py
@@ -0,0 +1,12 @@
+# Write a python function that takes a number as a parameter and returns number's square..
+# Also check if there is a built in function that does the  same
+
+number= int(input('Choose a number: '))
+# 1- using the power operator**
+print(number**2)
+
+# 2- multiply the number in itself
+print(number*number)
+ 
+ # 3- using the power built in function
+print(number.__pow__(2))
