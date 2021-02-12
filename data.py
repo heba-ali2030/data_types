a00d1e9d4ace4f0832bac96947cf8bbec2478904
heba-ali2030 <heba.galaa@gmail.com>
2021-02-13 00:05:28 +0200

    modify

--- data.py
+++ data.py
@@ -3,21 +3,13 @@
 # ask the user to write anumber
 number =int(input('please choose any number from your mind : '))
 
-# print(number/2)
-
-# print(type(number/2))
 print(number % 2)
-# if number / 2 not give % print it is an even number
-# if number/2 == 'classint'  :
-#     print('The number you choosed is an even number')
+
 if number % 2 == 0  :
     print('The number you choosed is an even number')
 
-# # if number / 2 give % print it is an even number
 else:
     print('The number you choosed is an odd number')
-
-
 
 
 
