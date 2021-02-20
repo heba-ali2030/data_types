589bc3aeb12adf1c902794075af7071538b1e12f
heba-ali2030 <heba.galaa@gmail.com>
2021-02-20 22:00:25 +0200

    add

---
+++ area_of_triangle.py
@@ -0,0 +1,11 @@
+# write a function def area_triangle(b,h) to compute the area of triangle : formula is area= s*b*h
+
+def area_triangle(b,h):
+
+    area= 0.5 * b * h
+    
+    print("the area of a triangle of base", b, "and height", h, "is", area)
+   
+
+area_triangle(3,5)
+# area_triangle(6,9)
--- tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-return temperature_conversion
